fe90c9efece90ec9596375deb592199610adf1b5
LouiseClarke97 <louiselikespie@live.ie>
2017-05-17 15:24:51 +0100

    Database files
    Within this folders are our database files.

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,40 @@
+# --- Sample dataset
+
+# --- !Ups
+
+insert into category (id,name) values ( 1,'Audio' );
+insert into category (id,name) values ( 2,'Books' );
+insert into category (id,name) values ( 3,'Computer' );
+insert into category (id,name) values ( 4,'Furniture' );
+insert into category (id,name) values ( 5,'Kitchen' );
+insert into category (id,name) values ( 6,'Laundry' );
+insert into category (id,name) values ( 7,'Mobile Phone' );
+insert into category (id,name) values ( 8,'Television' );
+
+insert into chart (id,song,artist,release) values ( 1,"Shape of you","Ed Sheeran","06/01/2017" );
+insert into chart (id,song,artist,release) values ( 2,"Galway girl","Ed Sheeran","17/03/2017" );
+insert into chart (id,song,artist,release) values ( 3,"Symphony","Clean Bandit ft. Zara Larsson","17/03/2017" );
+insert into chart (id,song,artist,release) values ( 4,"Humble", "Kendrick Lamar", "07/04/2017" );
+insert into chart (id,song,artist,release) values ( 5,"It ain't me", "Kygo & Selena Gomez", "16/02/2017" );
+insert into chart (id,song,artist,release) values ( 6,"Something just like this", "Chainsmokers & Coldplay", "22/02/2017" );
+insert into chart (id,song,artist,release) values ( 7,"Passionfruit", "Drake", "18/03/2017" );
+insert into chart (id,song,artist,release) values ( 8,"Stay", "Zedd & Alessia Cara", "23/02/2017" );
+insert into chart (id,song,artist,release) values ( 9,"Solo dance", "Martin Jensen", "04/11/2016" );
+insert into chart (id,song,artist,release) values ( 10,"Sign of the times", "Harry Styles", "31/03/2017" );
+
+insert into product (id,category_id,name,description,stock,price) values ( 1,5,'Kettle','Steel Electric Kettle',100,55.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 2,5,'Fridge freezer','Fridge + freezer large',45,799.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 3,1,'Portable Music Player','250GB music player (MP3,MP4,WMA,WAV)',5,99.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 4,3,'13inch Laptop','HP laptop,8GB RAM,250GB SSD',45,799.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 5,3,'8inch Tablet','Android 5.1 Tablet,32GB storage,8inch screen',5,99.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 6,8,'60inch TV','Sony 4K,OLED,Smart TV',12,5799.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 7,6,'Washing Machine','1600rpm spin,A+++ rated,10KG',50,699.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 8,7,'Phone','Windows 10,5.2inch OLED,3GB RAM,64GB Storage',45,799.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 9,3,'10inch Tablet','Windows 10,128GB storage,8inch screen',5,299.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 10,5,'Oven','Oven + Grill,Stainless Steel',10,399.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 11,4,'Bed','Super King size,super comfort mattress',5,899.00 );
+insert into product (id,category_id,name,description,stock,price) values ( 12,2,'Learning JavaScript','Become a JavaScript expert in 2 hours!',50,29.00 );
+
+insert into user (email,name,address,password) values ( 'admin@products.com', 'Alice Admin', 'password', 'admin' );
+insert into user (email,name,address,password) values ( 'manager@products.com', 'Bob Manager', 'password', 'manager' );
+insert into user (email,name,address,password) values ( 'customer@products.com', 'Charlie Customer', 'password', 'customer' );
